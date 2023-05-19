54e8c743e427e2ef29a813dcd71032db0a29c5ec
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-05-19 20:47:42 +0900

    fix: fix entry-vercel.ts

--- src/server/entry-vercel.ts
+++ src/server/entry-vercel.ts
@@ -1,4 +1,5 @@
 import { createMiddleware } from "@hattip/adapter-node";
+import { hattipApp } from "./hattip";
 
 // cf. https://github.com/hattipjs/hattip/blob/03a704fa120dfe2eddd6cf22eff00c90bda2acb5/packages/bundler/bundler-vercel/readme.md
 
