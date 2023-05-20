7729c713448b8ebf88bf18f3029d1cb6756dd5f9
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-05-20 21:40:17 +0900

    chore: comment

--- src/utils/config.tsx
+++ src/utils/config.tsx
@@ -2,7 +2,8 @@ import process from "node:process";
 import { z } from "zod";
 
 const Z_SERVER_CONFIG = z.object({
-  APP_REDIS_URL: z.string().default("redis://localhost:6379"),
+  APP_REDIS_URL: z.string().default("redis://localhost:6379/0"),
+  // manage prefix manually to (ab)use single upstash redis for staging and production
   APP_REDIS_PREFIX: z.string().default("dev"),
 });
 
