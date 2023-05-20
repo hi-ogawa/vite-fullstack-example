be858b51e2ed64067d3dca82cb589bfa1c647f96
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-05-20 11:40:49 +0900

    chore: setup access logger

--- src/server/hattip.ts
+++ src/server/hattip.ts
@@ -1,5 +1,6 @@
 import { type RequestHandler, compose } from "@hattip/compose";
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+import { logger } from "hono/logger";
 import { renderPage } from "vite-plugin-ssr/server";
 import { TRPC_ENDPOINT } from "../trpc/common";
 import { trpcRoot } from "../trpc/server";
@@ -36,4 +37,32 @@ const hattipTrpc: RequestHandler = (ctx) => {
   });
 };
 
-export const hattipApp = compose(hattipTrpc, hattipVitePluginSsr);
+function createHattipLogger() {
+  // borrow hono's logger with minimal compatibility hack
+  // https://github.com/honojs/hono/blob/0ffd795ec6cfb67d38ab902197bb5461a4740b8f/src/middleware/logger/index.ts
+  const honoLogger = logger();
+
+  const hattipLogger: RequestHandler = async (ctx) => {
+    let hattipRes!: Response;
+    const honoNext = async () => {
+      hattipRes = await ctx.next();
+    };
+    const honoReq = { method: ctx.method, raw: ctx.request };
+    const honoRes = {
+      get status() {
+        return hattipRes.status;
+      },
+    };
+    const honoCtx = { req: honoReq, res: honoRes };
+    await honoLogger(honoCtx as any, honoNext as any);
+    return hattipRes;
+  };
+
+  return hattipLogger;
+}
+
+export const hattipApp = compose(
+  createHattipLogger(),
+  hattipTrpc,
+  hattipVitePluginSsr
+);
