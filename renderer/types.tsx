c6ed1f5345097109ad06990075ff6797cddb7f61
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-05-21 09:30:28 +0900

    chore: simplify typing

--- renderer/types.tsx
+++ renderer/types.tsx
@@ -1,30 +1,19 @@
 import type React from "react";
+import type { escapeInject } from "vite-plugin-ssr/server";
 import type { PageContextBuiltInClientWithClientRouting } from "vite-plugin-ssr/types";
 
 //
 // page context
 //
 
-export type PageContext = IntersectionReplace<
-  PageContextBuiltInClientWithClientRouting,
-  {
-    Page: React.FC;
+export type PageContext =
+  PageContextBuiltInClientWithClientRouting<React.FC> & {
     pageProps?: Record<string, unknown>;
-  }
->;
+  };
 
 export type PageClientRender = (ctx: PageContext) => void;
 
 export type PageServerRender = (ctx: PageContext) => {
-  documentHtml: unknown;
+  documentHtml: ReturnType<typeof escapeInject>;
   pageContext: {}; // TODO: extra context can be passed to client (e.g. document title)
 };
-
-//
-// utils
-//
-
-// cf. https://github.com/sindresorhus/type-fest/blob/1fce25cf8afadd4d4013e8f0ee64d045eda45cd7/source/except.d.ts
-type StrictOmit<T, K extends keyof T> = Omit<T, K>;
-
-type IntersectionReplace<T1, T2> = StrictOmit<T1, keyof T1 & keyof T2> & T2;
