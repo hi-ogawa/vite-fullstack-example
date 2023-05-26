8f05fab08e4f91cce4e2c682550361496bde9313
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-05-26 23:46:32 +0900

    chore: rename dot-env.sh to dotenv

--- misc/dotenv.sh
+++ misc/dotenv.sh
@@ -3,10 +3,10 @@
 # based on https://stackoverflow.com/questions/19331497/set-environment-variables-from-file-of-key-value-pairs
 
 # usage:
-#   bash misc/dot-env.sh [dot-env-file] [command...]
+#   bash misc/dotenv.sh [env-file] [command...]
 
-dot_env_file="$1"
+env_file="$1"
 shift
 
 # shellcheck disable=SC2046
-env $(grep -v '^#' "$dot_env_file") "${@}"
+env $(grep -v '^#' "$env_file") "${@}"
